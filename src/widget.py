4e14ed77f87a367d87c380f3c2d9fed23eede293
aslanhaag <aslanhaag@gmail.com>
2025-01-25 14:19:11 +0300

    widget fixes

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,7 @@
-def mask_account_card(full_cart_name):
+from typing import Union
+
+
+def mask_account_card(full_cart_name: Union[str]) -> str:
     """Функция скрывает часть символов номера карты/счета"""
     if "Счет" in full_cart_name:
         return f"{full_cart_name[:4]} **{full_cart_name[-4:]}"
@@ -7,6 +10,5 @@ def mask_account_card(full_cart_name):
         return f"{full_cart_name[:-17]} {full_cart_name[-16:-12]} {full_cart_name[-12:-10]}** **** {full_cart_name[-4:]}"
 
 
-def get_date(date_str):
+def get_date(date_str: Union[str]) -> str:
     return f"{date_str[8:10]}.{date_str[5:7]}.{date_str[:4]}"
-
