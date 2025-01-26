15ed57698041535dc18329cbef5cdeb05f150e37
aslanhaag <aslanhaag@gmail.com>
2025-01-26 15:28:32 +0300

    main.py fix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src import masks
 
-print(masks.get_mask_card_number(""))
-print(masks.get_mask_account(""))
+user_input_card = input("Введите номер карты: ")
+user_input_mask = input("Введите номер счета: ")
+print(masks.get_mask_card_number(user_input_card))
+print(masks.get_mask_account(user_input_mask))
