2e4299952631eddfe11c557fe63a2924a7038cd7
aslanhaag <aslanhaag@gmail.com>
2025-02-06 22:35:55 +0300

    fix widget.py and add tests

--- src/widget.py
+++ src/widget.py
@@ -6,11 +6,12 @@ from src import masks
 def mask_account_card(full_cart_name: Union[str]) -> str:
     """Функция скрывает часть символов номера карты/счета"""
     if "Счет" in full_cart_name:
-        return masks.get_mask_account(full_cart_name)
+        return f'Счет {masks.get_mask_account(full_cart_name[-20:])}'
     else:
-
-        return masks.get_mask_card_number(full_cart_name)
-
+        if len(full_cart_name.split()) == 3:
+            return f"{full_cart_name.split()[-3]} {full_cart_name.split()[-2]} {masks.get_mask_card_number(full_cart_name.split()[-1])}"
+        else:
+            return f"{full_cart_name.split()[-2]} {masks.get_mask_card_number(full_cart_name.split()[-1])}"
 
 def get_date(date_str: Union[str]) -> str:
     """Функция конвертирует дату"""
