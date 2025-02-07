2a2991846bdfea33a807ee5dc28aab61bac2dc51
aslanhaag <aslanhaag@gmail.com>
2025-02-07 18:09:02 +0300

    fix tests names

---
+++ tests/test_widget.py
@@ -0,0 +1,32 @@
+from src.widget import get_date
+
+
+def test_get_data_normal(data_test):
+    assert get_date("2024-03-11T02:26:18.671407") == data_test
+
+
+def test_get_data_non_normal(data_test_non_standart):
+    assert get_date("дата - 11.03.2024") == data_test_non_standart
+
+
+import pytest
+
+from src.widget import mask_account_card
+
+
+def test_mask_account_card():
+    with pytest.raises(TypeError):
+        mask_account_card()
+
+
+@pytest.mark.parametrize(
+    "value, expect",
+    [
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+    ],
+)
+def test_mask_card_number(value, expect, card_data):
+    assert mask_account_card(value) == expect
