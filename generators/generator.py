e7baa1646383752c5dd95e1d8bfc9d00f52d9147
aslanhaag <aslanhaag@gmail.com>
2025-02-15 19:26:10 +0300

    hot fixes

--- generators/generator.py
+++ generators/generator.py
@@ -1,11 +1,11 @@
-
-
 def filter_by_currency(transactions, currency="USD"):
 
     for transaction in transactions:
-        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
+        if (
+            transaction.get("operationAmount", {}).get("currency", {}).get("code")
+            == currency
+        ):
             yield transaction
-
 
 
 def transaction_descriptions(transactions):
@@ -14,9 +14,10 @@ def transaction_descriptions(transactions):
 
 
 def card_number_generator(start, stop):
-    for number in range(start,stop + 1):
+    for number in range(start, stop + 1):
         card_number = f"{number:016d}"
         yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+
 
 for card_number in card_number_generator(0, 5):
     print(card_number)
