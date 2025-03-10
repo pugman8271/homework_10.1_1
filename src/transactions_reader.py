5850f1d4b90da2d60cf945407d6a6a548efc6d3d
aslanhaag <aslanhaag@gmail.com>
2025-03-10 23:00:00 +0300

    fixes test_transactions_reader.py

--- src/transactions_reader.py
+++ src/transactions_reader.py
@@ -1,17 +1,14 @@
 import csv
+
 import pandas as pd
 
 
-
-def  transactions_reader_csv(directory):
+def transactions_reader_csv(directory):
     with open(directory, encoding="utf-8") as csv_file:
-        reader = csv.DictReader(csv_file, delimiter=';')
+        reader = csv.DictReader(csv_file, delimiter=";")
         return list(reader)
 
 
-def  transactions_reader_xlsx(directory):
+def transactions_reader_xlsx(directory):
     reader_xlsx = pd.read_excel(directory)
     return reader_xlsx.to_dict(orient="records")
-
-
-
--- tests/test_transactions_reader.py
+++ tests/test_transactions_reader.py
@@ -1,18 +1,22 @@
-import os
 from unittest.mock import patch
+
 import pandas as pd
 
+from src.transactions_reader import (transactions_reader_csv,
+                                     transactions_reader_xlsx)
 
-from src.transactions_reader import transactions_reader_csv, transactions_reader_xlsx
 
-
-@patch('csv.DictReader')
+@patch("csv.DictReader")
 def test_transactions_reader_csv(mock_test):
-    mock_test.return_value = [{'id': 'test_id', 'state': 'test_state'}]
-    assert transactions_reader_csv('transactions.csv') == [{'id': 'test_id', 'state': 'test_state'}]
+    mock_test.return_value = [{"id": "test_id", "state": "test_state"}]
+    assert transactions_reader_csv("transactions.csv") == [
+        {"id": "test_id", "state": "test_state"}
+    ]
 
 
-@patch('src.transactions_reader.pd.read_excel')
+@patch("src.transactions_reader.pd.read_excel")
 def test_transactions_reader_xlsx(mock_test):
-    mock_test.return_value = pd.DataFrame([{'id': 'test_id', 'state': 'test_state'}])
-    assert transactions_reader_xlsx('transactions_excel.xlsx') == [{'id': 'test_id', 'state': 'test_state'}]
+    mock_test.return_value = pd.DataFrame([{"id": "test_id", "state": "test_state"}])
+    assert transactions_reader_xlsx("transactions_excel.xlsx") == [
+        {"id": "test_id", "state": "test_state"}
+    ]
